2f9fd98be26138b8f9a58c7e0564c097450b6ef8
Emmanuel Vinicius <emmanuel.vinicius.98@gmail.com>
2018-07-13 15:39:10 -0300

    Inserção do projeto MVC

---
+++ sistemaPerguntasWeb/sistemaPerguntasWeb/Controllers/PerguntasController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace sistemaPerguntasWeb.Controllers
+{
+    public class PerguntasController : Controller
+    {
+        // GET: Perguntas
+        public ActionResult Legislacao()
+        {
+            return View();
+        }
+    }
+}
