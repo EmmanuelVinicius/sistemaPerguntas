680799ee1ad143eb8663381b5a5292be3add24bc
Emmanuel Vinicius <emmanuel.vinicius.98@gmail.com>
2018-09-11 15:58:54 -0300

    Falta: Login e término de consulta de aulas. Parte estrutural

--- sistemaPerguntasWeb/sistemaPerguntasWeb/Scripts/JS_Home.js
+++ sistemaPerguntasWeb/sistemaPerguntasWeb/Scripts/JS_Home.js
@@ -11,6 +11,26 @@
         $(".modal-title").text($(this).text())
         $(".modal-body").text();
     });
-    $("#busca").quicksearch("html body p small font");
-    $("#consulta").quicksearch("table tr td");
+    //$("#busca").quicksearch("html body p small font");
+    $("#consulta").on('keyup', function () {
+        var value = $(this).val().toLowerCase();
+        $("table tbody tr td").filter(function () {
+            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+        });
+    });/*
+    $("#myInput").on("keyup", function () {
+        var value = $(this).val().toLowerCase();
+        $("#myTable tr").filter(function () {
+            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+        });
+    });*/
+    $("#txtBusca").keyup(function () {
+        var texto = $(this).val();
+
+        $("#ulItens li").css("display", "block");
+        $("#ulItens li").each(function () {
+            if ($(this).text().toUpperCase().indexOf(texto.toUpperCase()) < 0)
+                $(this).css("display", "none");
+        });
+    });
 })
