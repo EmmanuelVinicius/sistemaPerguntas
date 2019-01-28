147119fec71a3988bbb00527889f4890ca4b4dcb
Emmanuel Oliveira <emmanueloliveira@iusnatura.com.br>
2019-01-28 16:16:28 -0200

    Read das perguntas pronto

--- sistemaPerguntasWeb/sistemaPerguntasWeb/Controllers/PerguntasController.cs
+++ sistemaPerguntasWeb/sistemaPerguntasWeb/Controllers/PerguntasController.cs
@@ -1,7 +1,9 @@
-﻿using sistemaPerguntasWeb.Models;
+﻿using sistemaPerguntasWeb.App_Start;
+using sistemaPerguntasWeb.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 
@@ -10,12 +12,21 @@ namespace sistemaPerguntasWeb.Controllers
     public class PerguntasController : Controller
     {
         // GET: Perguntas
-        [HttpGet]
         public ActionResult Legislacao()
         {
             List<Perguntas> perguntas = new List<Perguntas>();
-            perguntas.Add(new Perguntas(1, "Quantas aulas é necessário para fazer o exame de direção?", "10", "20", "30", "40", "b"));
-            perguntas.Add(new Perguntas(2, "Quantas aulas é necessário para fazer a prova de legislação?", "15", "25", "35", "45", "d"));
+            var command = SQL.GetDataSet("SELECT * FROM Perguntas");
+            for (int i = 0; i < command.Tables[0].Rows.Count; i++)
+            {
+                perguntas.Add(new Perguntas());
+                perguntas[i].ID = (int)command.Tables[0].Rows[i]["ID"];
+                perguntas[i].Corpo = command.Tables[0].Rows[i]["Corpo"].ToString();
+                perguntas[i].OpcaoA = command.Tables[0].Rows[i]["OpcaoA"].ToString();
+                perguntas[i].OpcaoB = command.Tables[0].Rows[i]["OpcaoB"].ToString();
+                perguntas[i].OpcaoC = command.Tables[0].Rows[i]["OpcaoC"].ToString();
+                perguntas[i].OpcaoD = command.Tables[0].Rows[i]["OpcaoD"].ToString();
+                perguntas[i].Certo = command.Tables[0].Rows[i]["RespostaCerta"].ToString();
+            }
             return View(perguntas);
         }
         [HttpPost]
@@ -23,9 +34,10 @@ namespace sistemaPerguntasWeb.Controllers
         {
             int pontos = 0;
             model = new List<Perguntas>();
+            /*
             model.Add(new Perguntas(1, "Quantas aulas é necessário para fazer o exame de direção?", "10", "20", "30", "40", "b"));
             model.Add(new Perguntas(2, "Quantas aulas é necessário para fazer a prova de legislação?", "15", "25", "35", "45", "d"));
-
+            */
             for (int i = 0; i < model.Count; i++)
             {
                 var botao = Request.Form["radio" + i];
--- sistemaPerguntasWeb/sistemaPerguntasWeb/Models/Perguntas.cs
+++ sistemaPerguntasWeb/sistemaPerguntasWeb/Models/Perguntas.cs
@@ -7,22 +7,12 @@ namespace sistemaPerguntasWeb.Models
 {
     public class Perguntas
     {
-		public Perguntas(int _ID, string _corpo, string _opcao1, string _opcao2, string _opcao3, string _opcao4, string _certo)
-        {
-            ID = _ID;
-            Corpo = _corpo;
-            Opcao1 = _opcao1;
-            Opcao2 = _opcao2;
-            Opcao3 = _opcao3;
-            Opcao4 = _opcao4;
-            Certo = _certo;
-        }
         public int ID { get; set; }
         public string Corpo { get; set; }
-        public string Opcao1 { get; set; }
-        public string Opcao2 { get; set; }
-        public string Opcao3 { get; set; }
-        public string Opcao4 { get; set; }
+        public string OpcaoA { get; set; }
+        public string OpcaoB { get; set; }
+        public string OpcaoC { get; set; }
+        public string OpcaoD { get; set; }
         public string Certo { get; set; }
     }
 }
